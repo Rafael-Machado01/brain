0eb2f176bee2080ce001b5160b9b85f719fc5dfe
Rafael <rafael.machado.ampudiabr@gmail.com>
2025-04-14 21:16:26 -0300

    var new

--- TipsCode/Ejs/codes/notes/app.js
+++ TipsCode/Ejs/codes/notes/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs')
 app.listen( port, () => {
 console.log('O servidor está em', port);
 });
@@ -9,9 +10,13 @@ console.log('O servidor está em', port);
 app.get('/', (req,res) => {
    const date = new Date();
    let day = date.getDay();
+   let msg = ''
 if(day == 6 || day == 0) {
+   msg = 'Final de semana'
    res.send('Hoje é fim de semana! Para de trabalhar.')
+   
 }else {
-   res.send('Hoje é um dia de muito trabalho.')
+   msg = 'Dia de semana';
 }
+res.render('list', {msg : msg} )
 })
