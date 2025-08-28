040da241ab2a131b31180bf8fc0312661e17fe2c
Rafael Ampudia <70187787+Rafael-Machado01@users.noreply.github.com>
2025-08-27 21:31:23 -0300

    lista 2 zerada.

---
+++ Analise Desenvolvimento de Sistemas/Algoritmo/Código/C++/struct/lista2quatro.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+#define t 15
+
+struct dados {
+   float sell;
+   float cost;
+};
+
+dados v[t];
+
+void read();
+void result();
+
+int main() {
+   read();
+   result();
+}
+
+void read() {
+   for(int i=0;i<t;i++) {
+      cout << "Digite o custo do produto " << i+1 << endl;
+      cin >> v[i].cost;
+      v[i].sell = v[i].cost * 1.50;
+   }
+}
+
+void result() {
+   for(int i=0;i<t;i++) {
+      cout << "Produto " << i+1 << " Valor de venda é " << v[i].sell << endl;
+   }
+}
