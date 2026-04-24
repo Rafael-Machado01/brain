c66e2f2727303a16d9bd0711bc6f9fe9c67643f8
Rafael-Machado01 <rafael.machado.ampudia01@gmail.com>
2026-04-23 21:39:37 -0300

    test

--- readme/app/editor/page.tsx
+++ readme/app/editor/page.tsx
@@ -3,6 +3,8 @@ import Markdown from "react-markdown";
 import { useEffect, useState } from "react";
 export default function Editor() {
  const [content,setContent] = useState(``);
+ const [savedContent, setSavedContent] = useState(``);
+ const [tutor,setTutor] = useState(false);
  
  useEffect( () => {
   const saved = localStorage.getItem('save');
@@ -16,6 +18,7 @@ export default function Editor() {
     console.log(expire);
     if(now - parsed.saveAt < expire) {
       setContent(parsed.content);
+      setSavedContent(parsed.content);
     }else {
       localStorage.removeItem('save')
     }
@@ -24,9 +27,20 @@ export default function Editor() {
  
  function handleChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
   setContent(event.currentTarget.value);
+  if(tutor = true) {
+     const firstLetter = content.slice(0,1);
+  if(firstLetter == "#") {
+    alert("O # no Markdown serve para criar titulos");
+  }
+  }
  }
  function handleClick() {
   localStorage.setItem('save',JSON.stringify({content:content,saveAt: Date.now()}));
+  setSavedContent(content);
+  alert('Este editor foi salvo por 24hrs')
+ }
+ function handleTutor() {
+  setTutor(true);
  }
 
 
@@ -34,10 +48,13 @@ export default function Editor() {
     <>
     
 
-    <h1>Voce está editando</h1>
+    <h1>Voce está editando</h1> 
+    {content !== savedContent && <span>*</span>} 
     <textarea value={content} onChange={handleChange}></textarea>
     <Markdown>{content}</Markdown>
     <button onClick={handleClick}>salvar</button>
+    <button onClick={handleTutor}>Ativar modo tutor</button>
+    <p>tutor está:  {tutor}</p>
     </>
   )
 }
