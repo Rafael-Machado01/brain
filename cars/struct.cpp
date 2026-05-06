7781ed1978798ae68ee0c0a3c22bd9aef0ec2aef
Rafael-Machado01 <rafael.machado.ampudia01@gmail.com>
2026-05-06 20:02:21 -0300

    initial commit

--- cars/struct.cpp
+++ cars/struct.cpp
@@ -1,35 +1,72 @@
-// Exemplo de relacionamentos com struct.
-
 #include <iostream>
 using namespace std;
 
 struct Cidade {
     int id;
-    string nome;
-    string uf;
+    string name,uf;
 };
 
 struct Cliente {
+    int id,telefone;
+    string name,endereco;
+    int idCidade;
+};
+
+struct Veiculo {
+    string placa,modelo,marca;
+    int ano;
+    Cliente *idCliente;
+};
+
+struct Servico {
     int id;
-    string nome;
-    string endereco;
-    int telefone;
-    Cidade *cidade_id;
+    string descricao;
+    float maoDeObra;
+};
+
+struct Peca{
+    int id,qtdeEstoque,minimoEstoque;
+    string descricao;
+};
+
+struct Mecanico {
+    int id,telefone;
+    string nome,especialidade;
+};
+
+struct Ordem {
+    int id,qtdePeca;
+    string data;
+    Veiculo *placa;
+    Mecanico *idMecanico;
+    Servico *idServico;
+    Peca *idPeca;
 
 };
 
 int main() {
-    Cidade v[10];
-    Cliente c[10];
-    cout << "Digite o ID do cliente: " << endl;
-    cin >> c[0].id;
-    cout << "Digite o nome do cliente: " << endl;
-    cin >> c[0].nome;
-    cout << "Digite o endereco do cliente: " << endl;
-    cin >> c[0].endereco;
-    cout << "Digite o telefone do cliente: " << endl;
-    cin >> c[0].telefone;
-
-    c[0].cidade_id = &v[0];
-    // Então na função de cadastrar cliente passamos o valor do id x para o endereço &v[x];
+    Cidade c[2];
+    Cliente u[2];
+    for (int i=0; i < 2; i++) {
+        cout << "Digite o id da cidade " << endl;
+        cin >> c[i].id;
+        cout << "Digite o nome da cidade" << endl;
+        cin >> c[i].name;
+        cout << "Digite a UF:" << endl;
+        cin >> c[i].uf;
+    }
+    u[0].id = 1;
+    u[1].id = 2;
+    u[0].name = "Bernardo";
+    u[1].name = "Ju";
+    u[0].idCidade = 1;
+    u[1].idCidade = 2;
+    for (int i=0; i < 2; i++) {
+        cout << "ID:" << u[i].id << endl;
+        cout << "Nome:" << u[i].name << endl;
+        cout << "Endereço:" << u[i].endereco << endl;
+        cout << "Telefone:" << u[i].telefone << endl;
+        cout << "Cidade" << c[u[i].idCidade].name << endl;
+        cout << "UF:" << c[u[i].idCidade].uf << endl;
+    }
 }
